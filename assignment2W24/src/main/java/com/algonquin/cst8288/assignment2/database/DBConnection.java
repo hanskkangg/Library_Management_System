9e42b9777cd2f7ecbba87a8010c2339c9222ce90
hanskkangg <hans.kkang@gmail.com>
2024-02-16 19:35:04 -0500

    second commit - Implement singleton pattern for database connection.

--- assignment2W24/src/main/java/com/algonquin/cst8288/assignment2/database/DBConnection.java
+++ assignment2W24/src/main/java/com/algonquin/cst8288/assignment2/database/DBConnection.java
@@ -1,14 +1,37 @@
-package com.algonquin.cst8288.assignment2.database;
-
-import java.sql.Connection;
-
-public class DBConnection {
+	package com.algonquin.cst8288.assignment2.database;
 	
-	private static Connection connection = null;
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.SQLException;
 	
-	private String serverUrl = "jdbc:mysql://localhost:3306/bookvault";
-	private String userString = "***";
-	private String passwordString = "***";
-	private String driverString = "com.mysql.cj.jdbc.Driver";
-	
-}
+	public class DBConnection {
+		
+		private static Connection connection = null;
+		
+		private static String serverUrl = "jdbc:mysql://localhost:3306/bookvault";
+		private static String userString = "***";
+		private static String passwordString = "***";
+		private static String driverString = "com.mysql.cj.jdbc.Driver";
+		
+	    // Private constructor to prevent instantiation
+	    private DBConnection() {
+	    }
+	    // Public method to return the singleton Connection instance
+	    public static Connection getConnection() {
+	        if (connection == null) {
+	        	
+	            try {
+	                Class.forName(driverString); // Ensure the driver is registered
+	                connection = DriverManager.getConnection(serverUrl, userString, passwordString);
+	            } catch (ClassNotFoundException e) {
+	                System.out.println("MySQL JDBC Driver not found.");
+	                e.printStackTrace();
+	            } catch (SQLException e) {
+	                System.out.println("Connection to database failed.");
+	                e.printStackTrace();
+	            }
+	        }
+	        return connection;
+		
+	}
+	}
